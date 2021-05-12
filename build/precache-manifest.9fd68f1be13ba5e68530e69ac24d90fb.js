debd4f342d03ebb2537738d56f38ab8957239455
naldurien <nicole.aldurien@gmail.com>
2021-05-11 23:27:54 -0400

    deploying to firebase

---
+++ build/precache-manifest.9fd68f1be13ba5e68530e69ac24d90fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc307ab6658e3075a97a21acebb2439d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0ab5d83f1242c268384",
+    "url": "/static/css/main.92911422.chunk.css"
+  },
+  {
+    "revision": "f9248e1b45cb2aed097a",
+    "url": "/static/js/2.c3b490cd.chunk.js"
+  },
+  {
+    "revision": "f13b8ee62297cdeb55e53ad5d3f0bd7f",
+    "url": "/static/js/2.c3b490cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0ab5d83f1242c268384",
+    "url": "/static/js/main.15148241.chunk.js"
+  },
+  {
+    "revision": "f35af6f7ccead2fe5dbb",
+    "url": "/static/js/runtime-main.4422d9b2.js"
+  }
+]);
