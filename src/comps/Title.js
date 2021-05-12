b3b94a25d4cbf9f916dd8d386c1faaecdffe1bb2
naldurien <nicole.aldurien@gmail.com>
2021-05-11 21:16:18 -0400

    first commit

---
+++ src/comps/Title.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>HJC Tribute Gallery</h1>
+      <h2>Herbert John Craig</h2>
+      <h3>Gone But Never Forgotten</h3><br/>
+      <p>If you have photos to share, please upload them here.</p>
+    </div>
+  )
+}
+
+export default Title;
